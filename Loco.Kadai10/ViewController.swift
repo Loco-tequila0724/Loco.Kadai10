1f29128aedc11334597ab8c4fa009c6bbd3b0ac7
Loco-tequila0724 <hayato09097699090@icloud.com>
2021-07-08 17:38:06 +0900

    課題10　完成

--- Loco.Kadai10/ViewController.swift
+++ Loco.Kadai10/ViewController.swift
@@ -1,19 +1,26 @@
-//
-//  ViewController.swift
-//  Loco.Kadai10
-//
-//  Created by 日高隼人 on 2021/07/08.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet private weak var tableView: UITableView!
+    private let prefectureArray = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return prefectureArray.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifierArray = ["redCell", "greenCell", "blueCell"]
+        let identifier = identifierArray[indexPath.row % 3]
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as UITableViewCell
+
+        cell.textLabel?.text = String(prefectureArray[indexPath.row])
+        cell.detailTextLabel?.text = "\(String(indexPath.row + 1))番目の都道府県です。"
+        return cell
+    }
 }
-
